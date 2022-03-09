916bcf6192f63f11eb6e0b899a57c881201104f8
howard chen <chenhoward0401@gmail.com>
2022-03-09 19:14:07 +0800

    chore:[Howard] add charjs or echartjs

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ This template should help get you started developing with Vue 3 and Typescript i
 Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's `.vue` type support plugin by running `Volar: Switch TS Plugin on/off` from VSCode command palette.
 
 # 非常前期的階段，只有框，有空慢慢練習，目標全 tailwind 手刻
+
+## How to import d3js ?
+1. install d3,  @types/d3
+```
+npm install d3
+npm install --save @types/d3
+```
+2. add types to index.d.ts 
+https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/d3/index.d.ts
--- src/views/dashboard/table.vue
+++ src/views/dashboard/table.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex h-dash_board_body w-full bg-gray-600 -z-20"
+  <div class="h-dash_board_body w-full bg-gray-600 -z-20"
         @click="resetCurSmallIconHoverIdx();isSidebarSmallIconHovered = false;"
         >
     <div class="" :class="isHeaderOpen ? `w-main_content_left_normal duration-100`:
@@ -24,11 +24,15 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- <canvas id="echartJS" width="400" height="600"></canvas> -->
+    <canvas style="width: 10rem; height:10rem;" id="chartJS" width="10" height="10"></canvas>
     <div :class="isHeaderOpen ? `w-40 duration-100`:`w-20 duration-100`"></div>
-  </div>
 
+  </div>
+  
 </template>
 <script setup lang="ts">
+import { onMounted } from 'vue' 
 import {
   isHeaderOpen,
   isSidebarClicked,
@@ -42,4 +46,176 @@ import {
   filterTableData,
   onClickSidebar,
 } from '../../hooks/dashboard'
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { BarChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+} from 'echarts/components';
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+
+///
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle,
+  ChartItem
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+///
+
+
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+
+// 接下来的使用就跟之前一样，初始化图表，设置配置项
+onMounted(() => {
+    const mm = document.getElementById('echartJS')
+    console.log("mm",mm)
+
+    if (mm) {
+      
+    var myChart = echarts.init(mm);
+    myChart.setOption({
+        xAxis: {
+          data: ['A', 'B', 'C', 'D', 'E']
+        },
+        yAxis: {},
+        series: [
+          {
+            data: [10, 22, 28, 43, 49],
+            type: 'bar',
+            stack: 'x'
+          },
+          {
+            data: [5, 4, 3, 5, 10],
+            type: 'bar',
+            stack: 'x'
+          }
+        ]
+    });
+    }
+
+
+
+  
+  const ctx = document.getElementById('chartJS') as ChartItem
+  if(ctx) {
+    const chatJs = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        // scales: {
+        //     y: {
+        //         beginAtZero: true
+        //     }
+        // }
+        responsive: true,
+    }
+  });
+  }
+
+
+
+
+
+})
+
+
+
+
+
+
 </script>
